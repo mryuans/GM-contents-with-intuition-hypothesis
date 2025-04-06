fa4ad5f88aa66846d8fee8be2e19c4f595749cfb
mr.yuan <1456721128@qq.com>
2025-04-06 10:58:44 +0800

    Initial post

---
+++ PixelDot_and_ScoreFunction.py
@@ -0,0 +1,46 @@
+from manimlib import *
+
+
+class PixelDot(Scene):
+    def random_color(self):
+        return Color(hsl=(np.random.random(), 0.7, 0.65))
+    def construct(self):
+        axes = NumberPlane()
+        def func(p):
+            p = np.array(p)
+            x, y = p.T
+            dx = -x
+            dy = -y
+            vec = np.column_stack([dx, dy])
+            return vec
+        
+        field = VectorField(func, axes)
+        # self.add(field) If you want to add a score function here, please uncomment this line.
+        points_num = 100
+        step_scale = 0.05
+
+        particles = VGroup(*[
+            Dot(radius=0.05).set_color(self.random_color()) 
+            for _ in range(points_num)
+        ])
+        paths = VGroup(*[
+            VMobject().set_stroke(color=particle.get_color(), width=2, opacity=0.7)
+            for particle in particles
+        ])
+
+        self.add(particles, paths)
+        
+        # 定义更新函数（每个粒子独立更新）
+        def update_system(mobs, dt):
+            for particle, path in zip(particles, paths):
+                # 生成随机位移
+                dx = np.random.normal(0, step_scale) 
+                dy = np.random.normal(0, step_scale)
+                particle.shift(dx*RIGHT + dy*UP)
+                if len(path.get_all_points()) == 0:
+                    path.start_new_path(particle.get_center())
+                else:
+                    path.add_line_to(particle.get_center()).set_stroke(width=1.2)
+        particles.add_updater(update_system)
+        self.wait(15)
+        particles.remove_updater(update_system)
---
+++ PixelSpace.py
@@ -0,0 +1,35 @@
+from manimlib import *
+
+class PixelSpace(ThreeDScene):
+    def construct(self):
+        title = Text("A Piexl Space in m*n dimensions").set_color(BLACK).scale(0.5).to_corner(UR).fix_in_frame()
+        self.add(title)
+        axes = ThreeDAxes().set_color(BLACK)
+        self.add(axes)
+
+        vec1 = Vector(
+            [1, 1, 1]
+        ).set_color(BLUE)
+        label1 = Matrix([
+            ["a_{11}", "a_{12}", r"\cdots", "a_{1n}"],
+            ["a_{21}", "a_{22}", r"\cdots", "a_{2n}"],
+            [r"\vdots", r"\vdots", r"\ddots", r"\vdots"],
+            ["a_{m1}", "a_{m2}", r"\cdots", "a_{mn}"]
+        ]
+        ).next_to(vec1).set_color(BLACK).scale(0.5).fix_in_frame()
+        label_name = Text("Pixel matrix").set_color(BLACK).next_to(label1, UP, SMALL_BUFF).fix_in_frame().scale(0.5)
+        self.add(vec1, label1, label_name)
+
+        vec2 = Vector(
+            [1, -1, 1]
+        ).set_color(RED)
+        label2 = Matrix([
+            ["b_{11}", "b_{12}", r"\cdots", "b_{1n}"],
+            ["b_{21}", "b_{22}", r"\cdots", "b_{2n}"],
+            [r"\vdots", r"\vdots", r"\ddots", r"\vdots"],
+            ["b_{m1}", "b_{m2}", r"\cdots", "b_{mn}"]
+        ]
+        ).next_to(vec1, LEFT).set_color(BLACK).scale(0.5).fix_in_frame()
+        label_name_2 = Text("Judge matrix").set_color(BLACK).next_to(label2, UP, SMALL_BUFF).fix_in_frame().scale(0.5)
+        self.add(vec2, label2, label_name_2)
+
