2099e9cdebb3643ed3b77259b236ba4b50a42c39
mryuans <1456721128@qq.com>
2025-04-15 20:36:20 +0800

    Update PixelSpace.py
    Some usage update

--- PixelSpace.py
+++ PixelSpace.py
@@ -1,5 +1,6 @@
 from manimlib import *
 
+
 class PixelSpace(ThreeDScene):
     def construct(self):
         title = Text("A Piexl Space in m*n dimensions").set_color(BLACK).scale(0.5).to_corner(UR).fix_in_frame()
@@ -15,7 +16,9 @@ class PixelSpace(ThreeDScene):
             ["a_{21}", "a_{22}", r"\cdots", "a_{2n}"],
             [r"\vdots", r"\vdots", r"\ddots", r"\vdots"],
             ["a_{m1}", "a_{m2}", r"\cdots", "a_{mn}"]
-        ]
+        ],
+            ellipses_row=-2,
+            ellipses_col=-2
         ).next_to(vec1).set_color(BLACK).scale(0.5).fix_in_frame()
         label_name = Text("Pixel matrix").set_color(BLACK).next_to(label1, UP, SMALL_BUFF).fix_in_frame().scale(0.5)
         self.add(vec1, label1, label_name)
@@ -28,7 +31,9 @@ class PixelSpace(ThreeDScene):
             ["b_{21}", "b_{22}", r"\cdots", "b_{2n}"],
             [r"\vdots", r"\vdots", r"\ddots", r"\vdots"],
             ["b_{m1}", "b_{m2}", r"\cdots", "b_{mn}"]
-        ]
+        ],
+            ellipses_row=-2,
+            ellipses_col=-2
         ).next_to(vec1, LEFT).set_color(BLACK).scale(0.5).fix_in_frame()
         label_name_2 = Text("Judge matrix").set_color(BLACK).next_to(label2, UP, SMALL_BUFF).fix_in_frame().scale(0.5)
         self.add(vec2, label2, label_name_2)
